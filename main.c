1564b14fe0434f99a891f558b1a8b1d62b0024f0
r144 <renatinhu_barbosa@hotmail.com>
2016-08-28 18:21:23 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "listaDupla.h"
+#include "listaDupla.c"
 
 
 int main () {
@@ -9,14 +10,17 @@ int main () {
   insereDepois(l, l->head, aux= novoNo(12, NULL, NULL));
   aux2 = novoNo(21, NULL, NULL);
   insereDepois(l,aux,aux2);
-  aux = novoNo(42, NULL, NULL); 
+  aux = novoNo(42, NULL, NULL);
   insereDepois(l, aux2, aux);
   imprime(l);
   imprimeReverso(l);
-  imprime(BuscaMenor(ListaDupla l));
-  
+
+  printf("\no menor item eh: %d\n", BuscaMenor(l)->item);//imprime o menor valor
+  printf("\nlista ordenada\n");
+  ordena(l);
+  imprime(l);
   printf ("\nremovendo %d \n", aux2->item);
-  if((aux2 = buscar(l, 21) ) != NULL) 
+  if((aux2 = buscar(l, 21) ) != NULL)
     removeNo(aux2);
   imprime(l);
   destroiLista(l);
